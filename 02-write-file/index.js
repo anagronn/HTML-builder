3f3b3a8774bf8851831a4f1f05f9a4c436e6443d
anagronn <nastasya.vodchits@mail.ru>
2024-01-22 17:59:28 +0300

    implement second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
-const readline = require('readline');
 const path = require('path');
-const { stdin, stdout } = require('process');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const filePath = path.join(__dirname, 'newFile.txt');
 const writeStream = fs.createWriteStream(filePath);
-stdout.write("Hello, world!\n");
-stdin.on("data", function(data){
-  stdout.write(data);
-})
+rl.write("Hello, world!\nEnter some words, please\n");
+rl.on("line", function(input) {
+  const dataStringified = input.toString();
+  if (dataStringified === 'exit') {
+    writeStream.end();
+    rl.close();
+  } else writeStream.write(dataStringified + '\n');
+});
+
+rl.on('close', () => {
+  writeStream.end();
+  rl.write("Goodbye, world! The proccess is stopped");
+  process.exit();
+});
