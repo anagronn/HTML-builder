f4df5f4b958a8315c7265d0d0a4b33f482837917
anagronn <nastasya.vodchits@mail.ru>
2024-01-22 21:25:37 +0300

    feat: add task n4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,60 @@
+const fs = require('fs').promises;
+const path = require('path');
+const prevDirPath = path.join(__dirname, 'files');
+const newDirPath = path.join(__dirname, 'files-copy');
+
+async function isDirectoryExists(directory) {
+  try {
+    await fs.access(directory, fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function deleteFiles(directory) {
+  try {
+    const files = await fs.readdir(directory);
+    await Promise.all(files.map(file => fs.unlink(path.join(directory, file))));
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+async function createCopy(oldDir, newDir) {
+  try {
+    await fs.mkdir(newDir);
+
+    const files = await fs.readdir(oldDir);
+    await Promise.all(files.map(async file => {
+      const oldFile = path.join(oldDir, file);
+      const newFile = path.join(newDir, file);
+
+      const stats = await fs.stat(oldFile);
+
+      if (stats.isFile()) {
+        await fs.copyFile(oldFile, newFile);
+      }
+    }));
+    console.log(`Файлы успешно скопированы.`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+(async () => {
+  if (!(await isDirectoryExists(newDirPath))) {
+    await createCopy(prevDirPath, newDirPath);
+  } else {
+    if (await deleteFiles(newDirPath)) {
+      try {
+        await fs.rmdir(newDirPath);
+        await createCopy(prevDirPath, newDirPath);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+})();
